2aacbaf998d5a683096ce17a72de3209f2900d1d
wilsq <viljami.j.viinikainen@gmail.com>
2025-10-29 23:23:54 +0200

    First commit

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
