7bbcf36e20ddc9c837af1f88d2183e6f65ea0138
wilsq <viljami.j.viinikainen@gmail.com>
2025-10-31 11:45:52 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,13 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+  try {
+    await pool.query("SELECT NOW()");
+    console.log("✅ Connected to PostgreSQL");
+  } catch (err) {
+    console.error("❌ Database connection failed", err);
+  }
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
