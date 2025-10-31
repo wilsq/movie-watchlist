54ce27d6d5789da632ef7f64f5c918b55a0dc8a1
wilsq <viljami.j.viinikainen@gmail.com>
2025-10-31 10:55:14 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,25 @@ import dotenv from "dotenv";
 import { pool } from "./db.js";
 import movieRoutes from "./routes/movies.js";
 
+// lataa .env-tiedoston muuttujat
 dotenv.config();
+
+const app = express();
+
+// Middlewaret
+app.use(cors());
+app.use(express.json());
+
+// Testireitti
+app.get("/", (req, res) => {
+  res.send("Movie watchlist API is running");
+});
+
+app.use("/api/movies", movieRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("Server error", err);
+  res.status(500).json({ error: "Server error" });
+});
+
+const PORT = process.env.PORT || 5000;
